4dcb6056eee3b5b804121dbbbfba03a254b0a9d6
zakar <zakariya421@hotmail.com>
2024-03-18 15:05:45 +0100

    initial commit

---
+++ js_fundamentals/script.js
@@ -0,0 +1,19 @@
+// Declare and Assign a variable
+const isWaterWet = true;
+
+// Declare a variable and assign it later
+let isLoggedIn = null;
+console.log(isLoggedIn);
+isLoggedIn = true;
+
+let score = 1;
+score = score + 1;
+score = score - 1;
+score = score * 5;
+score =4;
+score = score ** 2;
+score = score % 3;
+
+
+let name = "Zakaria"
+console.log(name);
